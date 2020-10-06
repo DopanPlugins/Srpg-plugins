a07e06361ec6cffa4d666540ffecca2cede447a7
DopanPlugins <72324675+DopanPlugins@users.noreply.github.com>
2020-10-06 19:57:50 +0200

    Update SRPG_core (old 1.27).js
    -added boomys edits (Sv battler position Variables ; Counter if not SRPG mode fix ; Buff & States Core Fix)
    -added dopans edits (ActorBattleCommands )
    This SRPG Core is used in the "Updated-SRPG_Demo"

--- SRPG_core (old 1.27).js
+++ SRPG_core (old 1.27).js
@@ -152,6 +152,20 @@
  * @type boolean
  * @default true
  *
+ * // boomys edit start // side view battler positions
+ *
+ * @param ActorHomeX
+ * @desc This formula determines the actor's home X position in SRPG mode
+ * YEP Default: screenWidth - 16 - (maxSize + 2) * 32 + index * 32
+ * @default Graphics.width - 216 - index * 240
+ *
+ * @param ActorHomeY
+ * @desc This formula determines the actor's home Y position in SRPG mode
+ * YEP Default: screenHeight - statusHeight - maxSize * 48 + (index+1) * 48 - 32
+ * @default Graphics.height / 2 + 48 
+ *
+ * // boomys edit end // side view battler positions
+ *
  * @param enemyDefaultClass
  * @desc If you don't set class in enemy note, use this name.
  * @default Enemy
@@ -475,6 +489,20 @@
  * @desc 自分自身を対象とする行動では対象選択の処理をスキップします。(true / false)
  * @type boolean
  * @default true
+ * 
+ * // boomys edit start // side view battler positions
+ *
+ * @param ActorHomeX
+ * @desc アクタースプライトの基準位置
+ * YEP Default: screenWidth - 16 - (maxSize + 2) * 32 + index * 32
+ * @default Graphics.width - 216 - index * 240
+ *
+ * @param ActorHomeY
+ * @desc アクタースプライトの基準位置
+ * YEP Default: screenHeight - statusHeight - maxSize * 48 + (index+1) * 48 - 32
+ * @default Graphics.height / 2 + 48 
+ *
+ * // boomys edit end // side view battler positions
  *
  * @param enemyDefaultClass
  * @desc エネミーに職業（srpgClass）が設定されていない場合、ここの名前が表示されます。
@@ -689,11 +717,11 @@
     var _textSrpgTurnEnd = parameters['textSrpgTurnEnd'] || 'ターン終了';
     var _textSrpgAutoBattle = parameters['textSrpgAutoBattle'] || 'オート戦闘';
     var _srpgBattleQuickLaunch = parameters['srpgBattleQuickLaunch'] || 'true';
-    var _srpgActorCommandAttackSwitchID = Number(parameters['srpgActorCommandAttackSwitchID'] || 0);
-    var _srpgActorCommandSkillSwitchID = Number(parameters['srpgActorCommandSkillSwitchID'] || 0);
-    var _srpgActorCommandItemSwitchID = Number(parameters['srpgActorCommandItemSwitchID'] || 0);
-    var _srpgActorCommandEquipSwitchID = Number(parameters['srpgActorCommandEquipSwitchID'] || 0);
-    var _srpgActorCommandWaitSwitchID = Number(parameters['srpgActorCommandWaitSwitchID'] || 0);
+    var _srpgActorCommandAttackSwitchID = Number(parameters['srpgActorCommandAttackSwitchID'] || 0); //dopans edit
+    var _srpgActorCommandSkillSwitchID = Number(parameters['srpgActorCommandSkillSwitchID'] || 0); //dopans edit
+    var _srpgActorCommandItemSwitchID = Number(parameters['srpgActorCommandItemSwitchID'] || 0); //dopans edit
+    var _srpgActorCommandEquipSwitchID = Number(parameters['srpgActorCommandEquipSwitchID'] || 0); //dopans edit
+    var _srpgActorCommandWaitSwitchID = Number(parameters['srpgActorCommandWaitSwitchID'] || 0); //dopans edit
     var _srpgBattleEndAllHeal = parameters['srpgBattleEndAllHeal'] || 'true';
     var _srpgStandUnitSkip = 'true';
     var _srpgPredictionWindowMode = Number(parameters['srpgPredictionWindowMode'] || 1);
@@ -711,7 +739,9 @@
     var _srpgSet = parameters['srpgSet'] || 'srpg_set';
     var _rewardSe = parameters['rewardSound'] || 'Item3';
     var _expSe = parameters['expSound'] || 'Up4';
-
+    var _actorHomeX = parameters['ActorHomeX']) || Graphics.width - 216 - index * 240; // boomys edit
+    var _actorHomeY = parameters['ActorHomeY']) || Graphics.height / 2 + 48; // boomys edit 
+    
     var _Game_Interpreter_pluginCommand =
             Game_Interpreter.prototype.pluginCommand;
     Game_Interpreter.prototype.pluginCommand = function(command, args) {
@@ -3542,7 +3572,8 @@ Game_Interpreter.prototype.unitAddState = function(eventId, stateId) {
     var _SRPG_Sprite_Actor_setActorHome = Sprite_Actor.prototype.setActorHome;
     Sprite_Actor.prototype.setActorHome = function(index) {
         if ($gameSystem.isSRPGMode() == true) {
-            this.setHome(Graphics.width - 216 - index * 240, Graphics.height / 2 + 48);
+            //boomys edit // default was "this.setHome(Graphics.width - 216 - index * 240, Graphics.height / 2 + 48);"
+            this.setHome(eval(_actorHomeX), eval(_actorHomeY)); //boomys edit 
         } else {
             _SRPG_Sprite_Actor_setActorHome.call(this, index);
         }
@@ -6248,11 +6279,16 @@ Window_WinLoseCondition.prototype.refresh = function() {
             this._logWindow.push('pushBaseLine');
             if (Math.random() < this._action.itemCnt(target)) {
                 var attackSkill = $dataSkills[target.attackSkillId()]
-                if (target.canUse(attackSkill) == true) {
-                    this.invokeCounterAttack(subject, target);
-                } else {
-                    this.invokeNormalAction(subject, target);
-                }
+		        // boomys edit Start // fix counter if not using SRPG mode
+                if ($gameSystem.isSRPGMode() == true) {
+		            if (target.canUse(attackSkill) == true) {
+                        this.invokeCounterAttack(subject, target);
+                    } else {
+                        this.invokeNormalAction(subject, target);
+                    }
+		        } else {
+		            this.invokeCounterAttack(subject, target);	
+		        } // boomys edit end // fix counter if not using SRPG mode
             } else if (Math.random() < this._action.itemMrf(target)) {
                 this.invokeMagicReflection(subject, target);
             } else {

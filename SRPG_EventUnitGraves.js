f9f67836173856d5f40d85bf69b97d80c3bcd74d
DopanPlugins <72324675+DopanPlugins@users.noreply.github.com>
2021-11-24 22:29:31 +0100

    Update SRPG_EventUnitGraves.js

--- SRPG_EventUnitGraves.js
+++ SRPG_EventUnitGraves.js
@@ -515,13 +515,11 @@
 	
    // "this.unitRaise(eventID)"
    Game_Interpreter.prototype.unitRaise = function(eventID)) {
-       var eventID = eventId;
        var battleUnit = $gameSystem.EventToUnit(eventID);
        var eventUnit = $gameMap.event(eventID);
        if (battleUnit && (battleUnit[0] === 'actor')) {
           var actorID = battleUnit[1]._actorId;	       
-	  $gameSystem.eraseActorGrave(actorID)
-	       
+	  $gameSystem.eraseActorGrave(actorID) 
        };
        if (battleUnit && (battleUnit[0] === 'enemy')) {
 	  var unitID = eventUnit._eventEnemyUnitId;

73255165089916e63149fe8e22d79e5eff6e5508
DopanPlugins <72324675+DopanPlugins@users.noreply.github.com>
2021-11-28 03:32:35 +0100

    Update SRPG_core.js

--- SRPG_core.js
+++ SRPG_core.js
@@ -1,4 +1,4 @@
-﻿//=============================================================================
+//=============================================================================
 // SRPG_core.js -SRPGコンバータMV-
 // バージョン   : 1.32 + Q
 // 最終更新日   : 2020/10/8
@@ -311,6 +311,12 @@
  * @param textSrpgNone
  * @desc Term for nothing equipped on a unit.
  * @default None
+ *
+ * //dopan edit exa fix
+ * @param More Char Frame Usage
+ * @desc true if using more frames than default 3x4.true require 3 imgs '$srpg_set_E' '$srpg_set_X' '$srpg_set_A'
+ * @type boolean
+ * @default false
  *
  * @param srpgSet
  * @desc Spriteset for cursor and SRPG system icons
@@ -1093,7 +1099,7 @@
     var _actorHomeY = parameters['ActorHomeY'] || Graphics.height / 2 + 48;  //boomys edit
     var _enemyHomeX = parameters['EnemyHomeX'] || 216 + index * 240; //boomys edit
     var _enemyHomeY = parameters['EnemyHomeY'] || Graphics.height / 2 + 48; //boomys edit
-	
+    var _srpgEXA = parameters['srpgBattleSwitchID'] || 'false';	
 	
     var _Game_Interpreter_pluginCommand =
             Game_Interpreter.prototype.pluginCommand;
@@ -3983,24 +3989,42 @@ Game_Interpreter.prototype.unitAddState = function(eventId, stateId) {
                 if ((battlerArray[0] === 'actor' || battlerArray[0] === 'enemy') &&
                     battlerArray[1].isAlive()) {
                     if (battlerArray[1].isRestricted()) {
-                        var sx = (0 + this.characterPatternX()) * pw;//dopan edit info Default=> // var sx = (6 + this.characterPatternX()) * pw;
+                       //dopan edit info Default=> 'false'
+                        if (_srpgEXA ==='false') {
+                            var sx = (6 + this.characterPatternX()) * pw;  
+                        } else {var sx = (0 + this.characterPatternX()) * pw};
                         var sy = (0 + this.characterPatternY()) * ph;
                         this.createTurnEndSprites();
-                        this._turnEndSprite.bitmap = ImageManager.loadCharacter('$srpg_set_X'); //dopan edit info Default=> // this._turnEndSprite.bitmap = this._turnEndBitmap;
+                        //dopan edit info Default=> 'false' 
+                        if (_srpgEXA ==='false') {
+                            this._turnEndSprite.bitmap = this._turnEndBitmap;
+                        } else {this._turnEndSprite.bitmap = ImageManager.loadCharacter('$srpg_set_X')};
                         this._turnEndSprite.visible = true;
                         this._turnEndSprite.setFrame(sx, sy, pw, ph);
                     } else if (this.isTurnEndUnit() == true) {
-                        var sx = (0 + this.characterPatternX()) * pw; //dopan edit info Default=> // var sx = (3 + this.characterPatternX()) * pw;
+                        //dopan edit info Default=> 'false'
+                        if (_srpgEXA ==='false') {
+                            var sx = (3 + this.characterPatternX()) * pw;
+                        } else {var sx = (0 + this.characterPatternX()) * pw};
                         var sy = (0 + this.characterPatternY()) * ph;
                         this.createTurnEndSprites();
-                        this._turnEndSprite.bitmap = ImageManager.loadCharacter('$srpg_set_E'); //dopan edit info Default=> //this._turnEndSprite.bitmap = this._turnEndBitmap;
+                        //dopan edit info Default=> 'false'
+                        if (_srpgEXA ==='false') {
+                            this._turnEndSprite.bitmap = this._turnEndBitmap;
+                        } else {this._turnEndSprite.bitmap = ImageManager.loadCharacter('$srpg_set_E')};
                         this._turnEndSprite.visible = true;
                         this._turnEndSprite.setFrame(sx, sy, pw, ph);
                     } else if (battlerArray[1].isAutoBattle()) {
-                        var sx = (0 + this.characterPatternX()) * pw;//dopan edit info Default=> // var sx = (9 + this.characterPatternX()) * pw;
+                        //dopan edit info Default=> // 
+                        if (_srpgEXA ==='false') {
+                            var sx = (9 + this.characterPatternX()) * pw;
+                        } else {var sx = (0 + this.characterPatternX()) * pw};
                         var sy = (0 + this.characterPatternY()) * ph;
                         this.createTurnEndSprites();
-                        this._turnEndSprite.bitmap = ImageManager.loadCharacter('$srpg_set_A'); //dopan edit info Default=> // this._turnEndSprite.bitmap = this._turnEndBitmap;
+                        //dopan edit info Default=> // 
+                        if (_srpgEXA ==='false') {
+                            this._turnEndSprite.bitmap = this._turnEndBitmap;
+                        } else {this._turnEndSprite.bitmap = ImageManager.loadCharacter('$srpg_set_A')};
                         this._turnEndSprite.visible = true;
                         this._turnEndSprite.setFrame(sx, sy, pw, ph);
                     } else if (this._turnEndSprite) {
